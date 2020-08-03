01bf95cf10d342a7a5e707b1e4846573e6db0d26
echolimitless <echolimitless@outlook.com>
2020-08-03 23:05:57 +0900

    200803

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container Video");
+
+  //Sounds
+  const sound = document.querySelectorAll(".sound-picker button");
+};
+
+app();
