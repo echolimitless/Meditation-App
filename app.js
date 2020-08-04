8616136f8c6a9035cdbb197c6ab0752027611c71
echolimitless <echolimitless@outlook.com>
2020-08-05 00:51:31 +0900

    20200805

--- app.js
+++ app.js
@@ -5,7 +5,34 @@ const app = () => {
   const video = document.querySelector(".vid-container Video");
 
   //Sounds
-  const sound = document.querySelectorAll(".sound-picker button");
+  const sounds = document.querySelectorAll(".sound-picker button");
+  //Time Display
+  const timeDisplay = document.querySelectorAll(".time-display");
+  //Get the Length oof the outline
+  const outlineLength = outline.getTotalLength();
+  //Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+  });
+
+  //Create a function specific to stop and play yhe sounds
+  const checkPlaying = (song) => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./svg/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./svg/play.svg";
+    }
+  };
 };
 
 app();
